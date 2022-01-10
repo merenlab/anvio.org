ad0d46d344dc049691f7840365df1cc4706cfb93
A. Murat Eren <a.murat.eren@gmail.com>
2022-01-10 10:44:06 +0100

    uppity

--- _scripts/update-all.sh
+++ _scripts/update-all.sh
@@ -4,6 +4,11 @@ set -e
 
 _scripts/update-people-data.sh
 
-anvi-script-gen-help-pages -o help/main
+anvi-script-gen-help-pages -o help/main --quiet
+echo "Program & artifact help pages re-populated."
 
-anvi-script-gen-programs-network -o network/network.json
+anvi-script-gen-programs-network -o network/network.json --quiet
+echo "Anvi'o universe network is re-populated."
+
+echo
+echo "You're done here. Please double-check what you're about to commit."
--- help/main/index.md
+++ help/main/index.md
@@ -17,7 +17,7 @@ If you need an introduction to the terminology used in 'omics research or in anv
 <a href="/network/" target="_blank"><img src="/images/anvio-network.png" width="100%" /></a>
 
 {:.notice}
-The help contents were last updated on **10 Jan 22 10:35:43** for anvi'o version **7.1-dev (hope)**.
+The help contents were last updated on **10 Jan 22 10:42:57** for anvi'o version **7.1-dev (hope)**.
 
 
 {% include _project-anvio-version.html %}
