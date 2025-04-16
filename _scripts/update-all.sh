23e870c8a8e161b3ce570e83a42478bb3cb5f66c
A. Murat Eren (Meren) <a.murat.eren@gmail.com>
2025-04-16 15:45:20 +0200

    fixy fix

--- _scripts/update-all.sh
+++ _scripts/update-all.sh
@@ -2,17 +2,20 @@
 
 set -e
 
+echo "Re-populating tags under learn ..."
 _scripts/populate-tags.py
-echo "Tags under learn/ re-populated."
 
+echo "Re-populating people data ..."
 _scripts/update-people-data.sh
-echo "People data re-populated."
 
+echo "Re-populating program & artifact help pages ..."
 anvi-script-gen-help-pages -o help/main --quiet
-echo "Program & artifact help pages re-populated."
 
+echo "Re-populating anvi'o universe network ..."
 anvi-script-gen-programs-network -o network/network.json --quiet
-echo "Anvi'o universe network is re-populated."
 
 echo
-echo "You're done here. PLEASE DOUBLE-CHECK WHAT YOU'RE ABOUT TO COMMIT."
+echo
+echo "You're done here. PLEASE DOUBLE-CHECK WHAT YOU'RE ABOUT TO COMMIT and RUN THE JEKYLL SERVER TO SEE IF THERE ARE ERRORS"
+echo
+echo
